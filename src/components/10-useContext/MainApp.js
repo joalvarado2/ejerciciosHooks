e93ad27ec65e718ab5d1c6fb2537c4cae2a87bf8
joalvarado2 <jonathanalva92@gmail.com>
2021-07-21 21:47:57 -0500

    conociendo useContext

---
+++ src/components/10-useContext/MainApp.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AppRouter from './AppRouter'
+
+const MainApp = () => {
+    return (
+        <AppRouter/>
+    )
+}
+
+export default MainApp
---
+++ src/components/10-useContext/NavBar.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <NavLink exact activeClassName="active" className="nav-link" aria-current="page" to="/">
+                Home
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact activeClassName="active" className="nav-link" to="/about">
+                About
+              </NavLink>
+            </li>
+            <li className="nav-item">
+              <NavLink exact activeClassName="active" className="nav-link" to="/login">
+                Login
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
